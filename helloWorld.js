3239a04b1f5f827912cb4ffd4f139a9a6c373ebc
grandom <rezamoridi2042@gmail.com>
2023-03-09 04:23:02 +0330

    update

--- helloWorld.js
+++ helloWorld.js
@@ -4,6 +4,13 @@ function time() {
     var m = d.getMinutes();
     var h = d.getHours();
     return ("0" + h).substr(-2) + ":" + ("0" + m).substr(-2) + ":" + ("0" + s).substr(-2);
+   
 }
 setInterval(time, 1000);
 document.getElementById("demo").innerHTML=time();
+
+function hello() {
+    return "Hello"
+}
+
+document.getElementById("demo2").innerHTML=hello();
